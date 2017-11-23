de0f67fff52c18ef335b7a35a95ef7a1729bb534
S <s>
2017-11-23 14:49:57 -0800

    {"types":"C","runs":59}

--- Kareldolph.java
+++ Kareldolph.java
@@ -8,9 +8,9 @@ public class Kareldolph extends GCompound{
 	public Kareldolph() {
 		
 		body = new GRect(BODY_WIDTH, BODY_HEIGHT);
-		body.setColor(Color.WHITE);
+		body.setColor(cream);
 		body.setFilled(true);
-		body.setFillColor(Color.WHITE);
+		body.setFillColor(cream);
 		add(body, 0, 0);
 		
 	}
@@ -19,5 +19,7 @@ public class Kareldolph extends GCompound{
 	private static final int BODY_HEIGHT = 30;
 	
 	private GRect body;
+	
+	Color cream = Color.decode("#F0ECEB");
 	
 }
