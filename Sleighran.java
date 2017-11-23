e31fdb00b1901fb0ad7e2e8317b936b40a59e840
S <s>
2017-11-23 14:23:56 -0800

    {"types":"C","runs":34}

--- Sleighran.java
+++ Sleighran.java
@@ -40,7 +40,7 @@ public class Sleighran extends GCompound{
 		legs.setColor(sweater);
 		legs.setFilled(true);
 		legs.setFillColor(sweater);
-		add(legs, mehran.getWidth() + BODY_WIDTH + LEG_SIZE / 2, BODY_HEIGHT);
+		add(legs, mehran.getWidth() + BODY_WIDTH * 2, BODY_HEIGHT);
 	
 		add(mehran, mehran.getWidth(), 0 - mehran.getHeight());
 		
