08f90e3af72dcaa84086d3d2746596e67d5d1402
S <s>
2017-11-23 13:22:57 -0800

    {"types":"C","runs":99}

--- Sleighran.java
+++ Sleighran.java
@@ -61,7 +61,7 @@ public class Sleighran extends GCompound{
 		
 		skiLine = new GLine(0, 0, 0, SKI_LINE_LENGTH);
 		skiLine.setColor(gold);
-		add(skiLine, ARC_RADIUS, BACK_HEIGHT + ARC_RADIUS);
+		add(skiLine, ARC_RADIUS + SKI_LINE_LENGTH, BACK_HEIGHT);
 		
 	}
 	
