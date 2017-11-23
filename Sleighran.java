fa2955b8232f3b0086877a49de702078a73cd42b
S <s>
2017-11-23 12:46:34 -0800

    {"types":"C","runs":48}

--- Sleighran.java
+++ Sleighran.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+
+import acm.graphics.GArc;
 import acm.graphics.GCompound;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
@@ -7,29 +9,40 @@ public class Sleighran extends GCompound{
 
 	public Sleighran() {
 		
-		base = new GRect(BASE_WIDTH, BASE_HEIGHT);
-		base.setColor(red);
-		base.setFilled(true);
-		base.setFillColor(red);
+		back = new GArc(2 * ARC_RADIUS, 2 * ARC_RADIUS, 20, 90);
+		back.setColor(red);
+		back.setFilled(true);
+		back.setFillColor(red);
+		add(back, 0, 0);
 		
-		bottomLeft = new GOval(BOTTOM_LEFT_RADIUS * 2, BOTTOM_LEFT_RADIUS * 2);
-		bottomLeft.setColor(red);
-		bottomLeft.setFilled(true);
-		bottomLeft.setFillColor(red);
-		
-		add(base, 0, 0);
-		add(bottomLeft, 0 - BOTTOM_LEFT_RADIUS, 0 - (BOTTOM_LEFT_RADIUS * 2 - BASE_HEIGHT));
+//		base = new GRect(BASE_WIDTH, BASE_HEIGHT);
+//		base.setColor(red);
+//		base.setFilled(true);
+//		base.setFillColor(red);
+//		
+//		bottomLeft = new GOval(BOTTOM_LEFT_RADIUS * 2, BOTTOM_LEFT_RADIUS * 2);
+//		bottomLeft.setColor(red);
+//		bottomLeft.setFilled(true);
+//		bottomLeft.setFillColor(red);
+//		
+//		add(base, 0, 0);
+//		add(bottomLeft, 0 - BOTTOM_LEFT_RADIUS, 0 - (BOTTOM_LEFT_RADIUS * 2 - BASE_HEIGHT));
 		
 	}
 	
+//	GArc arc = new GArc(2 * r, 2 * r, -60, -60);   // creates an upward-facing arc against house
+//	add(arc, cx - r, cy - r - SIDE_HEIGHT / 2);   // adds arc to correct place on screen
+	private static final int ARC_RADIUS = 10;
 	
-	private static final int BASE_WIDTH = 50;
-	private static final int BASE_HEIGHT = 15;
-	private static final int BOTTOM_LEFT_RADIUS = 9;
+//	private static final int BASE_WIDTH = 50;
+//	private static final int BASE_HEIGHT = 15;
+//	private static final int BOTTOM_LEFT_RADIUS = 9;
 	
-	private GRect base;
+//	private GRect base;
+//	
+//	private GOval bottomLeft;
 	
-	private GOval bottomLeft;
+	private GArc back;
 /*
  * colors added for Sleighran compound
  */
