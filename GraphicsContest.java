bde1827dae530b069d926830555c08b037ba8b1a
S <s>
2017-11-23 11:06:41 -0800

    {"types":"C","runs":4}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -3,23 +3,17 @@
  * --------------------------
  */
 
-import java.awt.AlphaComposite;
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 
-import acm.graphics.GImage;
-import acm.graphics.GRect;
 import acm.program.*;
 import acm.util.*;
 
@@ -41,23 +35,30 @@ public class GraphicsContest extends GraphicsProgram {
 	private static final int BOTTOM_SPACE = 30;
 	
 	private static final int NUM_HOUSE_COLORS = 6;
-	
-	private boolean gameOver;   // initialized false
-	private boolean started;    // initialized false
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	ArrayList<House> houses;
 
 	public void run() {
 		setup();
+		waitForPlayer();
 	}
-	
 	
 	private void setup() {
 		addBackground();
 		createHouses();	
 		addMouseListeners();
 		addActionListeners();
+	}
+	
+	private void waitForPlayer() {
+		GLabel start = new GLabel("Click to start");  // creates starting prompt
+		start.setColor(Color.WHITE);
+		start.setFont("Arial - 28");
+		add(start, getWidth() / 2 - start.getWidth() / 2, getHeight() / 2 - 30); // add label
+		waitForClick();  // waits for player's click
+		remove(start);  // removes label from screen
 	}
 	
 	private void addBackground() {	
@@ -69,7 +70,6 @@ public class GraphicsContest extends GraphicsProgram {
 		try {
 			logo = ImageIO.read(input);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
