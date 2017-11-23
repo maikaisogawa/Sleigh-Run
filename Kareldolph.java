94efc886aed90712d94b10697d22f121f7d30a94
S <s>
2017-11-23 14:57:53 -0800

    {"types":"C","runs":62}

--- Kareldolph.java
+++ Kareldolph.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 
 import acm.graphics.GCompound;
+import acm.graphics.GPolygon;
 import acm.graphics.GRect;
 
 public class Kareldolph extends GCompound{
@@ -13,13 +14,30 @@ public class Kareldolph extends GCompound{
 		body.setFillColor(cream);
 		add(body, 0, 0);
 		
+		rightTriangle = makeRightTriangle(TRIANGLE, TRIANGLE);
+		rightTriangle.setColor(sky);
+		rightTriangle.setFilled(true);
+		rightTriangle.setFillColor(sky);
+		add(rightTriangle, BODY_WIDTH - TRIANGLE, 0);
+		
+	}
+	
+	private GPolygon makeRightTriangle(double width, double height){ 
+		GPolygon poly = new GPolygon();
+		poly.addVertex(0, 0);
+		poly.addVertex(width, 0);
+		poly.addVertex(width, height);
+		return poly;
 	}
 
 	private static final int BODY_WIDTH = 20;
 	private static final int BODY_HEIGHT = 30;
+	private static final int TRIANGLE = 5;
 	
 	private GRect body;
+	private GPolygon rightTriangle;
 	
 	Color cream = Color.decode("#F0ECEB");
+	Color sky = Color.decode("#1E2B55");
 	
 }
