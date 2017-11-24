e1f9137f092103d2d637be0269350b851be3b7b4
S <s>
2017-11-24 13:02:46 -0800

    {"types":"C","runs":51}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -107,8 +107,8 @@ public class GraphicsContest extends GraphicsProgram {
 	
 	private void finalScore() {
 		finalScore = new GLabel("Your score was: " + score);
-		finalX = getWidth() / 2 - finalScore.getWidth() / 2 * 2;
-		finalY = getHeight();
+		finalX = getWidth() / 2 - finalScore.getWidth() / 2 * 3;
+		finalY = finalScore.getHeight();
 		finalScore.setColor(Color.WHITE);
 		finalScore.setFont(new Font("Arial", Font.BOLD, 38));
 		finalScore.setLocation(scoreX,scoreY);
