c5bacf9636a57b559946544b14bd7c3c37af1d79
S <s>
2017-11-23 20:20:58 -0800

    {"types":"C","runs":105}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -77,7 +77,7 @@ public class GraphicsContest extends GraphicsProgram {
 	private static final double GRAVITY = 1;
 	
 	private double xVel = 0;
-	private double yVel = 1;
+	private double yVel = 2;
 	
 	private double kareldolphX = PARTY_SPACE * 8;
 	private double kareldolphY = HEIGHT / 2;
