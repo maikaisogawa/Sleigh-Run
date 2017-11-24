2bff1ac3be123423fa6b750e93108338a2562035
S <s>
2017-11-23 19:59:52 -0800

    {"types":"C","runs":84}

--- Rope.java
+++ Rope.java
@@ -6,7 +6,7 @@ import acm.graphics.GCompound;
 public class Rope extends GCompound {
 
 	public Rope() {
-		ropeStart = new GArc(2 * RADIUS, 2 * RADIUS, 20, 100);
+		ropeStart = new GArc(2 * RADIUS, 2 * RADIUS, 20, 90);
 		ropeStart.setColor(gold);
 		add(ropeStart, 0, 0);
 	}
