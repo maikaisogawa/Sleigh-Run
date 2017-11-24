75dc59ca65edd9034d5c12aa2e52ffd9f12fb631
S <s>
2017-11-23 21:55:56 -0800

    {"types":"C","runs":164}

--- House.java
+++ House.java
@@ -140,6 +140,7 @@ public class House extends GCompound {
 		double puffHeight = 0;
 		for(int i = 0; i < x; i++) {
 			smokePuff = new GImage(puff);
+			smokePuff.scale(0.2);
 			add(smokePuff, SIDE_WIDTH + HOUSE_WIDTH / 2 + 17 - smokePuff.getWidth() / 2, 0 - smokePuff.getHeight() - 5 - puffHeight);
 			puffHeight += smokePuff.getHeight() + 30;
 		}
