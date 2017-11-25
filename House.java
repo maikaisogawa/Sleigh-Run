25f0a10e16655806818806954947c29795a8ef64
S <s>
2017-11-24 20:44:14 -0800

    {"types":"C","runs":20}

--- House.java
+++ House.java
@@ -41,7 +41,7 @@ public class House extends GCompound {
 		
 		// fence of house
 		fence = new GRect(FENCE_LENGTH, FENCE_HEIGHT);
-		fence.setColor(Color.BLACK.brighter());
+		fence.setColor(Color.BLACK.brighter().brighter().brighter());
 		fence.setFilled(true);
 		fence.setFillColor(Color.LIGHT_GRAY);
 		
